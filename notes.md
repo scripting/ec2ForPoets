6a1eb6234185bfe20368b1d936878aaec4b87a1f
Dave Winer <dave.winer@gmail.com>
2015-08-02 17:34:47 -0400

    v0.41

--- notes.md
+++ notes.md
@@ -8,3 +8,27 @@ rm -rf ec2ForPoets
 
 git clone https://github.com/scripting/ec2ForPoets.git
 
+cd ec2ForPoets/synchttp
+
+pwd
+
+Running shell commands from Node
+
+https://github.com/arturadib/shelljs
+
+Watch log
+
+sudo tail -f /var/log/upstart/synchttp.log
+
+To cause an update to happen
+
+in frontier
+
+nodeEditorSuite.utilities.buildEc2PoetsIndexJson ()
+
+in terminal
+
+cd /volumes/baltimore/ec2forpoets/distribution
+
+aws s3 sync . s3://demo.forpoets.org/distribution
+
---
+++ startup.js
@@ -0,0 +1,25 @@
+function runUnixCommand (theCommand, callback) {
+	if (theCommand.length > 0) {
+		var exec = require ("child_process").exec;
+		console.log ("runUnixCommand: " + theCommand);
+		exec (theCommand, function (error, stdout, stderr) {
+			if (callback !== undefined) {
+				callback (error, stdout, stderr);
+				}
+			});
+		}
+	}
+function runCommands (s) {
+	var theList = s.split ("\n");
+	
+	function doCommand (ix) {
+		if (ix < theList.length) {
+			runUnixCommand (theList [ix], function (error, stdout, stderr) {
+				doCommand (ix + 1);
+				});
+			}
+		}
+	doCommand (0);
+	
+	}
+runCommands ("cd /home/ubuntu/ec2ForPoets/river4\nforever start river4\n");
